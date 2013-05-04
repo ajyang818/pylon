f1373c2008e04bbe33f634e59cee2fe738cf789f
Allen Yang <allen@yipit.com>
2013-05-04 14:47:25 -0400

    Setting up the local database

--- apps/tweet/models.py
+++ apps/tweet/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Tweet(models.Model):
+
+    author = models.CharField(max_length=50)
+    content = models.CharField(max_length=180)
+    date_published = models.DateTimeField(auto_now=True)
--- settings/settings.py
+++ settings/settings.py
@@ -1,3 +1,5 @@
+import os
+
 # Django settings for Pylon project.
 
 DEBUG = True
@@ -11,13 +13,12 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        #'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        #'NAME': '/Users/Allen/Learning/Twyttlr/Twyttlr.db',                      # Or path to database file if using sqlite3.
-        'NAME': 'twyttlrdb_mysql',                      # Or path to database file if using sqlite3.
-        'USER': 'allen',                      # Not used with sqlite3.
-        'PASSWORD': 'qweqwe',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'ENGINE': 'django.db.backends.mysql',  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),  # Or path to database file if using sqlite3.
+        'NAME': 'pylon_local',  # Or path to database file if using sqlite3.
+        'USER': 'root',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
+        'HOST': '127.0.0.1',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
