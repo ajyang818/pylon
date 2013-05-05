842a082b1f563d0cfb42a4ecce9ae60ea8df63b5
Allen Yang <allen@yipit.com>
2013-05-04 21:23:17 -0400

    Setting up media folder to allow images on templates

--- settings/settings.py
+++ settings/settings.py
@@ -71,13 +71,14 @@ STATIC_ROOT = ''
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
-STATIC_URL = '/static/'
+STATIC_URL = '/media/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    os.path.join(PROJECT_ROOT, "media"),
 )
 
 # List of finder classes that know how to find static files in
@@ -132,6 +133,7 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
+    'south',
     'tweet',
 )
 
